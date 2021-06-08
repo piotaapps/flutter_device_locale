6484e58ab6225dd174c08da792fe89960144d5ea
Paul Ryan <paul@sharpred.com>
2021-06-08 17:32:25 +0100

    null safety

--- lib/src/method_channel.dart
+++ lib/src/method_channel.dart
@@ -8,7 +8,7 @@ class FlutterDeviceLocaleMethodChannelPlugin
     extends FlutterDeviceLocalePlatform {
   @override
   Future<List<String>> deviceLocales() async {
-    final List<dynamic> result = await _channel.invokeMethod('deviceLocales');
-    return result.cast<String>();
+    final List<dynamic>? result = await _channel.invokeMethod('deviceLocales');
+    return result!.cast<String>();
   }
 }
